5aad789f6ea684cadee86d02e25e109f3a4e1b22
Frank Wei <frank@frankwei.xyz>
2021-01-13 17:32:27 +0800

    refactor: delete sensitive modules

--- backend/app/controller/announcement.js
+++ backend/app/controller/announcement.js
@@ -13,21 +13,5 @@ class AnnouncementController extends Controller {
       data: result,
     };
   }
-  /** 发布一条公告 */
-  async post() {
-    const { ctx } = this;
-    const { title, content, postId = 0, informInstant = 1, informNewUser = 0, expireTime } = ctx.request.body;
-    const result = await ctx.service.announcement.post(ctx.user.username, title, content, postId, informInstant, informNewUser, expireTime)
-    if(result) ctx.body = ctx.msg.success;
-    else ctx.body = ctx.msg.failure;
-  }
-
-  async delete() {
-    const { ctx } = this;
-    const { id } = ctx.params;
-    const result = await ctx.service.announcement.delete(id);
-    if(result) ctx.body = ctx.msg.success;
-    else ctx.body = ctx.msg.failure;
-  }
 }
 module.exports = AnnouncementController;
