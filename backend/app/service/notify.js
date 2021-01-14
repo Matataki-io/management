5b725f6e020641db1ccc1bc893dc43a22c104c22
xiaotiandada <952822399@qq.com>
2021-01-14 13:02:37 +0800

    feat: fix 用户文章推荐功能

--- backend/app/service/notify.js
+++ backend/app/service/notify.js
@@ -11,9 +11,9 @@ const ACTION_TYPES = [
   'comment', // 评论
   'like', // 点赞
   'reply', // 回复
-  'follow', //关注
+  'follow', // 关注
   'annouce', // 宣布
-  'transfer'
+  'transfer',
 ];
 
 /** 对象类型 */
@@ -25,19 +25,19 @@ const OBJECT_TYPES = [
   'announcementToken', // 引用内容为Fan票的公告
   'tokenWallet', // Token钱包
   'cnyWallet', // CNY钱包
-  'featuredArticles' // 精选文章
+  'featuredArticles', // 精选文章
 ];
 
 const isValidActionAndObject = (action, objectType) => ACTION_TYPES.includes(action) && OBJECT_TYPES.includes(objectType);
 
 class notifyService extends Service {
 
-  /*********/
+  /** *******/
   /** Set **/
-  /*********/
+  /** *******/
 
-  /** 
-   * 创建一个事件 
+  /**
+   * 创建一个事件
    * @uid 产生这个事件的用户
    * @action 用户所做的行为
    * @objectId 对象的索引
@@ -46,7 +46,7 @@ class notifyService extends Service {
    * @return 事件在数据库中的索引
    */
   async createEvent(uid, action, objectId, objectType, remark = null) {
-    if(!isValidActionAndObject(action, objectType)) return false;
+    if (!isValidActionAndObject(action, objectType)) return false;
 
     const result = await this.ctx.model.query(`
       INSERT INTO ${EVENT_TABLE} (user_id, action, object_id, object_type, remark, create_time)
@@ -59,22 +59,22 @@ class notifyService extends Service {
         object_id: objectId,
         object_type: objectType,
         remark,
-        create_time: moment().utc().format('YYYY-MM-DD HH:mm:ss')
-      }
-    })
-    return result[0]
+        create_time: moment().utc().format('YYYY-MM-DD HH:mm:ss'),
+      },
+    });
+    return result[0];
   }
 
-  /** 
+  /**
    * 设定事件的接收者 (一个事件多个接收者)
    * @eventId 事件在数据库中的索引
    * @uids 事件接收者列表
    */
   async setEventRecipients(eventId, uids) {
-    if(!uids || uids.length < 1) return false
+    if (!uids || uids.length < 1) return false;
     let sql = `INSERT INTO ${EVENT_RECIPIENT_TABLE} (event_id, user_id) VALUES (?, ?)`;
-    let recipients = [];
-    uids.forEach((uid, index) =>  {
+    const recipients = [];
+    uids.forEach((uid, index) => {
       recipients.push(eventId);
       recipients.push(uid);
       if (index > 0) sql += ', (?, ?)';
@@ -84,17 +84,16 @@ class notifyService extends Service {
     try {
       const result = await this.ctx.model.query(sql, {
         raw: true,
-        replacements: recipients
+        replacements: recipients,
       });
       return result;
-    }
-    catch(e) {
+    } catch (e) {
       this.logger.error(e);
       return false;
     }
   }
 
-  /** 
+  /**
    * 发送一个事件 (整合了创建事件与设定接收者)
    * @senderId 产生这个事件的用户
    * @receivingId 事件接收者
@@ -104,9 +103,12 @@ class notifyService extends Service {
    * @remark 【可选】补充信息
    * @noDuplication 【默认：true】避免重复。开启时，如果参数相同的事件已经存在，将不会创建新事件。
    */
-  async sendEvent(senderId, receivingIds,  action, objectId, objectType, remark, noDuplication = true) {
+  async sendEvent(senderId, receivingIds, action, objectId, objectType, remark, noDuplication = true) {
+
+    console.log('receivingIds', receivingIds);
+
     // 参数相同的事件如果已经存在了，就不会在创建新的
-    if(noDuplication) {
+    if (noDuplication) {
       let sql = `
         SELECT * FROM ${EVENT_TABLE}
         WHERE user_id = :senderId
@@ -114,25 +116,25 @@ class notifyService extends Service {
           AND object_id = :objectId
           AND object_type = :objectType
       `;
-      let replacements = {
+      const replacements = {
         senderId,
         action,
         objectId,
-        objectType
-      }
+        objectType,
+      };
 
       if (remark) {
         sql += 'AND remark = :remark';
         replacements.remark = remark;
       }
-      const existing = await this.ctx.model.query(sql, { raw: true, replacements })
-      console.log('existing', existing)
-      if(existing[0].length > 0) return false
+      const existing = await this.ctx.model.query(sql, { raw: true, replacements });
+      console.log('existing', existing);
+      if (existing[0].length > 0) return false;
     }
 
     // 创建事件
     const eventId = await this.createEvent(senderId, action, objectId, objectType, remark);
-    if(!eventId) return false;
+    if (!eventId) return false;
     // 设定事件的接收者
     const result = await this.setEventRecipients(eventId, receivingIds);
     return !!result[1];
