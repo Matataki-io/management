1055ab5f5ee6a3c61672456f01c834944939564a
Frank Wei <frank@frankwei.xyz>
2021-01-18 17:57:06 +0800

    feat: link to check out on BSCScan

--- frontend-ng/src/views/login/notification.vue
+++ frontend-ng/src/views/login/notification.vue
@@ -11,6 +11,7 @@
           <li>这是一个预览版，暂时仅限 META 代币持有者参与</li>
           <li>需要把 MetaMask 钱包切换到BSC网络</li>
           <li>需要你把 META 提现到 BSC 主网的私人钱包，并在下方参与 META 抵押锁仓30天，以获得30天的访问权限</li>
+          <li>BSC 主网的META代币地址为： <b>0xacae234ea5fddd1657038b7aa6b597664056c954</b></li>
           <li>社区教程：👉 <a href="https://matataki.io/p/5834" target="_blank" style="color: #0088ff;">如何将FAN票提现到BSC？ ↗️</a> 👈 </li>
           <li>META 抵押锁仓有效期30天，到期可以解押提现离场，或手动申请延长锁仓30天</li>
           <li>治理委员会仍在开发中，未来可能会增删部分功能</li>
