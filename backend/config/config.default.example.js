afebaa9e3df41efa7cc28391810fdacaca341c32
xiaotiandada <istianlei@qq.com>
2022-03-22 17:21:43 +0800

    feat: env config

---
+++ backend/config/config.default.example.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1552895551811_5889';
+
+  config.security = {
+    csrf: {
+      enable: false,
+      ignoreJSON: true, // 默认为 false，当设置为 true 时，将会放过所有 content-type 为 `application/json` 的请求
+    },
+    domainWhiteList: '*',
+  };
+  config.cluster = {
+    listen: {
+      path: '',
+      port: 7003,
+      hostname: '0.0.0.0',
+    },
+  };
+  config.cors = {
+    origin: '*',
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS',
+    credentials: true,
+  };
+
+  config.proxy = true;
+
+  // add your middleware config here
+  config.middleware = [ 'formatMessage' ];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
